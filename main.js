fc6018401a5496709818d03094cc3f6813cb2ab3
180mcoding <84143799+180mcoding@users.noreply.github.com>
2022-03-26 11:15:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("my first console project");
+var first_name="Marco";
+var last_name="Cota";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+var sum=a+b;
+console.log(sum);
+var sub=a-b;
+console.log(sub);
+var mult=a*b;
+console.log(mult);
+var div=a/b;
+console.log(div);
